efbb9630019299f263a8120ab2dd70c5a0a1c42b
pongsathonwon <men1234-2008@hotmail.com>
2024-11-19 15:11:31 +0700

    extract card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,32 @@
+import { Data } from "../../types/types";
+import Content from "../Content/Content";
+import Header from "../Header/Header";
+import Social from "../Social/Social";
+import Stat from "../Stat/Stat";
+import "./style.css";
+
+type CardProps = Data;
+
+function Card({
+  id,
+  name,
+  logo,
+  roundSeats,
+  score,
+  faculty,
+  likes,
+}: CardProps) {
+  return (
+    <div className="frame">
+      <Header {...{ name, logo, faculty }} />
+      <div className="linebreak"></div>
+      <Content {...{ roundSeats, score }} />
+      <div className="linebreak"></div>
+      <Stat />
+      <div className="linebreak"></div>
+      <Social likes={likes} />
+    </div>
+  );
+}
+
+export default Card;
