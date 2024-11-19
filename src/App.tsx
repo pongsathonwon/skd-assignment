c718d8db3f8030719f9fc5a0a74ff42e0e1e3f73
pongsathonwon <men1234-2008@hotmail.com>
2024-11-19 10:51:01 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Header } from "./components";
+function App() {
+  return (
+    <div className="frame">
+      <Header />
+      <div className="linebreak"></div>
+      <div>content session here</div>
+      <div className="linebreak"></div>
+      <div>score here</div>
+      <div className="linebreak"></div>
+      <div>social here</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import Hearticon from "../Icon/hearticon";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <img
+          src="img/engi.png"
+          srcSet="img/engi@2x.png 2x,img/engi@3x.png 3x"
+          className="header-left-img"
+        />
+        <div className="header-left-text">
+          <span className="header-left-line1 header-left-text-ali">
+            คณะวิศวกรรมศาสตร์
+          </span>
+          <span className="header-left-line2 header-left-text-ali">
+            สาขาวิศวกรรมทั่วไป
+          </span>
+          <span className="header-left-line3 header-left-text-ali">
+            จุฬาลงกรณ์หมาวิทยาลัย
+          </span>
+        </div>
+      </div>
+      <div className="header-right">
+        <button className="icon-btn">
+          <Hearticon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
